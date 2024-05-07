ede1d77388d095963d54a9fc03b46ba053a3639a
vytzab <vytautas.zabielskas@gmail.com>
2024-05-07 13:18:01 +0300

    Initial commit

---
+++ src/test/java/lt/vytzab/recipes/RecipesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vytzab.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
